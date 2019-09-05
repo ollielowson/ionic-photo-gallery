9d9f50bde0fe2c74229f420f253a3e5781ebee40
Adam Selley <a.selley@system15.co.uk>
2019-09-05 10:52:28 +0100

    Updated karma

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var Server = require('karma').Server;
-gulp.task('unittest', function(done){
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done).start();
-});
